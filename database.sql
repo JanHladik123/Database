0ea4a10cb5c8e36cf5ba3ca9782bd544142a05b6
JanHladik123 <60381843+JanHladik123@users.noreply.github.com>
2020-09-30 11:33:35 +0200

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,41 @@
+
+
+/* CREATE TABLE  cars (
+    id_car int,
+    manufacturer varchar (255),
+    model varchar (255),
+    license_plate varchar (255),
+    vintage smallint
+); */
+
+
+/*
+ALTER TABLE cars
+CHANGE COLUMN vintage TO year_of_manufacturer smallint;*/
+
+/*
+INSERT INTO cars
+VALUES (
+    1,
+    'Ford'
+    'Mustang'
+    '9B1 1042'
+    1986
+); */
+
+
+SELECT license_plate, manufacturer
+FROM cars
+
+
+
+
+CREATE  TABLE garages
+id_garage int AUTO_INCREMENT,
+color varchar (255),
+adress varchar (255),
+
+CREATE TABLE garages
+id_garage int NOT NULL AUTO_INCREMENT
+color varchar (255),
+adress varchar (255),
