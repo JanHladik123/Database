d4e23f1fec23065de7e9570a8a519f045c587e18
JanHladik123 <60381843+JanHladik123@users.noreply.github.com>
2021-01-26 10:06:28 +0100

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,19 +1,19 @@
 
 
-/* CREATE TABLE  cars (
+ CREATE TABLE  cars (
     id_car int,
     manufacturer varchar (255),
     model varchar (255),
     license_plate varchar (255),
     vintage smallint
-); */
+); 
 
 
-/*
+
 ALTER TABLE cars
-CHANGE COLUMN vintage TO year_of_manufacturer smallint;*/
+CHANGE COLUMN vintage TO year_of_manufacturer smallint;
 
-/*
+
 INSERT INTO cars
 VALUES (
     1,
@@ -21,7 +21,7 @@ VALUES (
     'Mustang'
     '9B1 1042'
     1986
-); */
+); 
 
 
 SELECT license_plate, manufacturer
